8703a0283edd8ea320c81f77493888f812930670
daw2-rojas15 <agus_bdn_94@hotmail.com>
2015-05-20 20:25:53 +0200

    prgit-rojas15 v5 fet amb w7 amb Git Gui

--- mitja.c
+++ mitja.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
 int main() {
 	float suma=0.0;
 	float mitja=0.0;
 	float nou_valor=0.0;
 	int numero_repeticions=0;
-	char consulta='s';
+	char consulta='y';
 	
-	while(consulta=='s') {
+	while(consulta=='y') {
 		printf("Dòna la següent dada: ");
 		scanf("%f",&nou_valor);
 		suma=suma+nou_valor;
 		numero_repeticions=numero_repeticions+1;
-		printf("Vols continuar (s/n): ");
-		scanf(" %s",&consulta); 
+		printf("Vols continuar (y/n): ");
+		scanf(" %y",&consulta); 
 	}
 	mitja=suma/numero_repeticions;
 	printf("La mitja és: %.2f\n",mitja);	
